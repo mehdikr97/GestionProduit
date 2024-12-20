69b096cfdc0989e591f4ee7c4d8b5c7b25e3a6d1
mehdi karkori <mehdikarkouri7@gmail.com>
2024-12-20 16:00:11 +0100

    creer la function ajouter

--- enventory.js
+++ enventory.js
@@ -6,4 +6,27 @@ class inventory {
   constructor() {
     this.produits = [];
     this.nextId = 1;
-  } }
+  } 
+  ajouter_unproduit() {
+    this.nextId = this.nextId++;
+    const name = prompt("Entrer le nom de produit :");
+    const description = prompt("Entrer la Description de produit :");
+    const quantity = parseInt(prompt("Entrer la quantite de produit :"));
+    const price = parseFloat(prompt("Entrer le prix de produit :"));
+    const product = new Product(name, description, quantity, price);
+    this.produits.push(product);
+    fs.writeFileSync('produits.json', JSON.stringify(this.produits, null, 2))
+    console.log("produit enregistree")
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
