930b342e6a7a7008ef2c563a163005068ae1dc6a
mehdi karkori <mehdikarkouri7@gmail.com>
2024-12-20 15:53:10 +0100

    Creer la class produit

---
+++ product.js
@@ -0,0 +1,11 @@
+class Product {
+    constructor( name, description, quantity, price) {
+
+      this.name = name;
+      this.description = description;
+      this.quantity = quantity;
+      this.price = price;
+    }
+  }
+  
+  module.exports = Product;
