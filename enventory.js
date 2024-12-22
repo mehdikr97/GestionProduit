8ef2fec0cb98f3beb149fe36dfb19d19605b2fe7
mehdi karkori <mehdikarkouri7@gmail.com>
2024-12-22 15:40:23 +0100

    Gérez les cas où un produit demandé

--- enventory.js
+++ enventory.js
@@ -6,32 +6,32 @@ class inventory {
   constructor() {
     this.produits = [];
     this.nextId = 1;
-  } 
+  }
   ajouter_unproduit() {
     this.nextId = this.nextId++;
     let name;
 
     do {
-        name = prompt("Entrez le nom du produit :")
-        if (!name) {
-            console.log("Le nom du produit ne peut pas être vide. Veuillez réessayer.");
-        }
+      name = prompt("Entrez le nom du produit :")
+      if (!name) {
+        console.log("Le nom du produit ne peut pas être vide. Veuillez réessayer.");
+      }
     } while (!name);
-        const description = prompt("Entrer la Description de produit :");
-        let quantity;
-        do {
-            quantity = parseInt(prompt("Entrez la quantité de produit :"));
-            if (isNaN(quantity) || quantity <= 0) {
-                console.log("entrer une quantite valide ");
-            } 
-        } while (isNaN(quantity) || quantity <= 0);
-    let price ;
+    const description = prompt("Entrer la Description de produit :");
+    let quantity;
+    do {
+      quantity = parseInt(prompt("Entrez la quantité de produit :"));
+      if (isNaN(quantity) || quantity <= 0) {
+        console.log("entrer une quantite valide ");
+      }
+    } while (isNaN(quantity) || quantity <= 0);
+    let price;
     do {
       price = parseFloat(prompt("Entrez le prix de produit :"));
       if (isNaN(price) || price <= 0) {
-          console.log("entrer un prix valide ");
+        console.log("entrer un prix valide ");
       }
-  } while (isNaN(price) || price <= 0);
+    } while (isNaN(price) || price <= 0);
     const product = new Product(name, description, quantity, price);
     this.produits.push(product);
     fs.writeFileSync('produits.json', JSON.stringify(this.produits, null, 2))
@@ -42,33 +42,48 @@ class inventory {
     this.produits = JSON.parse(data);
     console.log("Liste de produits");
     this.produits.forEach((product, index) => {
-      console.log(` ${index + 1} - Nom: ${product.name}, 
-    Description ${product.description},
-    Quantite : ${product.quantity}, Prix ${product.price}`)
+    console.log(` ${index + 1} - Nom: ${product.name}, 
+    -Description ${product.description},
+    -Quantite : ${product.quantity},
+    -Prix ${product.price}`)
     })
   }
   suprimmerProduit() {
     console.log("1. Supprimer un produit par ID");
     let id = prompt("Entrez l'ID du produit à supprimer : ");
-    this.produits.splice(id - 1, 1);
-    console.log("produit supprime")
-    fs.writeFileSync("produits.json", JSON.stringify(this.produits));
+    this.produits = JSON.parse(fs.readFileSync('produits.json', 'utf8'));
+
+    if(id>=1 && id<=this.produits.length){
+      this.produits.splice(id - 1, 1);
+      console.log("produit supprime")
+      fs.writeFileSync("produits.json", JSON.stringify(this.produits));
+    }else{
+      console.log("rien suprime")
+    }
+
   }
   modifierProduit() {
     console.log("1. Modifier un produit par ID");
     let id = parseInt(prompt("Entrez l'ID du produit à modifier : "));
-
-    let products = JSON.parse(fs.readFileSync('produits.json', 'utf8'));
-    const resultat = products[id -1];
-
+    this.produits = JSON.parse(fs.readFileSync('produits.json', 'utf8'));
+    if(id >=1 && id<=this.produits.length){ 
+    const resultat = this.produits[id - 1];
     resultat.name = prompt("Entrer le nouveau nom ")
-    resultat.description = prompt("Entrer la nouvelle description ")
-    resultat.quantity = parseInt(prompt("Entrer la nouvelle quantité "))
-    resultat.price = parseFloat(prompt("Entrer le nouveau prix "))
-//    products.push(resultat)
-    fs.writeFileSync('produits.json', JSON.stringify(products), 'utf8');
-  }}
-  const inva = new inventory();
+      resultat.description = prompt("Entrer la nouvelle description ")
+      resultat.quantity = parseInt(prompt("Entrer la nouvelle quantité "))
+      resultat.price = parseFloat(prompt("Entrer le nouveau prix "))
+      console.log("produit modifier")
+      //    products.push(resultat)
+      fs.writeFileSync('produits.json', JSON.stringify(this.produits), 'utf8');
+    }else {
+      console.log("il n ya rien bro")
+    }
+  }
+
+  }
+
+
+const inva = new inventory();
 while (true) {
   console.log("1-Ajouter un produit")
   console.log("2-afficher les produit")
@@ -89,7 +104,7 @@ while (true) {
       break;
     case '4': inva.modifierProduit()
       break;
-    case '5': return
+    case '5': return console.log("By bro");
     default: console.log("choix invalide")
   }
 }
